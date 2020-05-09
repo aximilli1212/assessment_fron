25d28bea10e6feb368b0370d9fea31e774196468
Aximilli1212 <aximill1212@gmail.com>
2020-05-09 12:14:28 +0000

    add default book image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,40 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="display-1 font-weight-bold mb-3">
-          Welcome to Lybrarie Bookshop
-        </h1>
-        <p class="subheading grey--text font-weight-regular">
-          All your books in one place.
-        </p>
-      </v-col>
-    </v-row>
+  <v-layout>
+    <v-flex md8 offset-md-2>
+      <v-container>
+        <v-row class="text-center">
+          <v-col class="mb-4">
+            <h1 class="display-1 font-weight-bold mb-3">
+              Welcome to Lybrarie Bookshop
+            </h1>
+            <p class="subheading grey--text font-weight-regular">
+              All your books in one place.
+            </p>
+          </v-col>
+        </v-row>
 
-    <section>
-      <v-row  justify="center">
-        <v-col xs="8" class="my-4">
-          <v-row justify="center" v-if="loadingTrending">
-            <v-progress-circular indeterminate size="72" color="primary"></v-progress-circular>
-          </v-row>
-          <v-row justify="center" class="my-n12" v-else>
-            <v-col v-for="(book, i) in bookList" :key="i" xs="6" sm="3" class="mx-0">
-              <book-card :book_details="book"> </book-card>
+        <section>
+          <v-row  justify="center">
+            <v-col xs="8" class="my-4">
+              <v-row justify="center" v-if="loadingTrending">
+                <v-progress-circular indeterminate size="72" color="primary"></v-progress-circular>
+              </v-row>
+              <v-row justify="center" class="my-n12" v-else>
+                <v-col v-for="(book, i) in bookList" :key="i" xs="6" sm="3" class="mx-0">
+                  <book-card :book_details="book"> </book-card>
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
-        </v-col>
-      </v-row>
-    </section>
+        </section>
 
 
 
 
-  </v-container>
+      </v-container>
+    </v-flex>
+  </v-layout>
+
 </template>
 
 <script>
